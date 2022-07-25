22c08c2f85066a3134f588cdc21be2ae045b7274
Bayu R.A <bayurahman497@gmail.com>
2022-07-26 00:04:46 +0700

    belajar state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Coba dong</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        name: 'Jusak',
+        age: '27',
+        gender: 'Laki - Laki',
+      }
+    }
+  }
+
+  componentDidMount = async () => {}
+
+  editData = () => {
+    const newData = {
+      name: 'Andrean',
+      age: '30',
+      gender: 'Laki - Laki'
+    }
+    this.setState({ person: newData });
+  }
+
+  render() {
+    return (
+       <div>
+        <h1>Nama : {this.state.person.name}</h1>
+        <h1>Age : {this.state.person.age}</h1>
+        <h1>Gender : {this.state.person.gender}</h1>
+
+        <button onClick={() => this.editData()}>
+          Ubah Data
+        </button>
+       </div>
+    );
+  }
 }
 
 export default App;
