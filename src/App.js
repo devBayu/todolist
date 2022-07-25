9c135a76b76ec1fe5c63d74e085ef53010363c67
Bayu R.A <bayurahman497@gmail.com>
2022-07-26 00:27:26 +0700

    hfakshd

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 0,
+      value: 1,
     }
   }
 
   componentDidMount = async () => {}
-
-  editData = () => {
-    const newData = {
-      name: 'Andrean',
-      age: '30',
-      gender: 'Laki - Laki'
-    }
-    this.setState({ person: newData });
-  }
 
   render() {
     return (
